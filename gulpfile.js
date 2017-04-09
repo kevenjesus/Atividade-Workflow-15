5991872b2cfb851d7abd0efd5167a33092c8cdd7
brennolemos <brenomiros@gmail.com>
2017-04-09 15:15:33 -0300

    finaliza

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,27 @@
 var gulp = require("gulp");
 var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
+var sass = require("gulp-sass");
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
 var watch = require('gulp-watch');
+
+
+gulp.task('minifica-css', function () {
+  return gulp.src('source/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility:'*'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('minifica-html', function() {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('source/index.html', ['minifica-html']);
+    gulp.watch('source/scss/*.scss', ['minifica-css']);
+});
